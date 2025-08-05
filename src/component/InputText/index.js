df2ec7e6e165f35f3e2ed2791854f6954c19a3d6
Bruno C <brunoramoscorreia15@gmail.com>
2025-08-04 23:04:32 -0300

    Primeiro Commit

---
+++ src/component/InputText/index.js
@@ -0,0 +1,12 @@
+import './InputText.css'
+
+function InputText(){
+    return(
+        <div>
+            <label> Nome</label>
+            <input />
+        </div>
+    )
+}
+
+export default InputText
